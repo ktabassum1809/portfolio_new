0a8bae70ea4251f5f3644e95b2c0e6017bee5167
ktabassum1809 <ktabassum1809@gmail.com>
2024-11-26 08:50:06 +0100

    added resume link

--- src/components/Navbar/Herosection/Bio.js
+++ src/components/Navbar/Herosection/Bio.js
@@ -10,7 +10,7 @@ const Bio = {
       "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
     github: "https://github.com/ktabassum1809",
     resume:
-      "https://drive.google.com/file/d/1-LDPbhM-GMgBcnY1hS8ZKGcspjjJYT09/view",
+      "https://drive.google.com/file/d/1YDZErI3fwoK8dK-09RZ-5OKUHFiqo6ve/view?usp=sharing",
     linkedin: "https://www.linkedin.com/in/tabassum-khan-3688602b7/",
    
   };
